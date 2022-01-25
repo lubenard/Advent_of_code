1f6c942e5daeb3dd87298342b56fa0ced7426299
lubenard <lubenard@student.42.fr>
2022-01-25 20:24:25 +0100

    Added first part of Exo 01

---
+++ Exo01/script.py
@@ -0,0 +1,14 @@
+file = open("input",'r')
+first_number = int(file.readline())
+increased = 0
+cur_number = 0
+
+print("First number is " + str(first_number))
+
+for line in file:
+    cur_number = int(line)
+    if cur_number > first_number:
+        increased = increased + 1
+    first_number = cur_number
+
+print("Increased = " + str(increased))
